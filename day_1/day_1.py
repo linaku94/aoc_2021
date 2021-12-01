c8c11f6e8e01d813e4a77da3d10de09d84065b68
Lina Kuhn <lina.kuhn@ymail.com>
2021-12-01 13:39:22 +0100

    timer

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -11,13 +11,15 @@ def window_sum(depths):                ### sum windows
         new_depths.append(sum(depths[i:i+3]))
     return new_depths
 
-filename = 'input.txt'
+if __name__ == '__main__':
+    
+    filename = 'input.txt'
 
-depths = []
-with open(filename, 'r') as file:
-    for line in file:
-        depths.append(int(line.rstrip()))
+    depths = []
+    with open(filename, 'r') as file:
+        for line in file:
+            depths.append(int(line.rstrip()))
 
-### first/second part
-print(f'first part: {depth_increases(depths)}'
-      f'\nsecond part: {depth_increases(window_sum(depths))}')
+    ### first/second part
+    print(f'first part: {depth_increases(depths)}'
+        f'\nsecond part: {depth_increases(window_sum(depths))}')
