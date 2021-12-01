706de36925a8105ebffd132c31958d19a788719f
Lina Kuhn <lina.kuhn@ymail.com>
2021-12-01 07:48:15 +0100

    day one

---
+++ day_1/day_1.py
@@ -0,0 +1,26 @@
+def depth_increases(depths):            ### check if depth increases
+    counter = 0
+    for i, d in enumerate(depths[1:]):
+        if d > depths[i]:
+            counter +=1
+    return counter
+
+def window_sum(depths):                ### sum windows
+    new_depths = []
+    for i, d in enumerate(depths[:-2]):
+        new_depths.append(sum(depths[i:i+3]))
+    return new_depths
+
+filename = 'input.txt'
+# file = open(filename, 'r')
+
+depths = []
+with open(filename, 'r') as file:
+    for line in file:
+        depths.append(int(line.rstrip()))
+
+### first part
+print('first part:', depth_increases(depths))
+
+## second part
+print('second part:', depth_increases(window_sum(depths)))
