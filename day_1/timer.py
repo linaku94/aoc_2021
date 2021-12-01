a143288e5368cc07961f63dd53a043c91df0fb3b
Lina Kuhn <lina.kuhn@ymail.com>
2021-12-01 13:39:03 +0100

    timer

---
+++ day_1/timer.py
@@ -0,0 +1,18 @@
+import time
+from day_1 import depth_increases, window_sum
+
+# runs = int(input('number of runs: '))
+runs = 100
+
+filename = 'input.txt'
+
+depths = []
+with open(filename, 'r') as file:
+    for line in file:
+        depths.append(int(line.rstrip()))
+
+start = time.monotonic()
+for i in range(runs):
+    depth_increases(window_sum(depths))
+
+print((time.monotonic()-start)/ runs)
