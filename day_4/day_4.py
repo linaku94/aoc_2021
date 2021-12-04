69ca358f1d2dea67b8aa1d34379069038a61b4b5
Lina Kuhn <lina.kuhn@ymail.com>
2021-12-04 16:12:23 +0100

    shorter

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -2,22 +2,18 @@ import numpy as np
 
 def play_game(boards, masks, draws):
     for i, draw in enumerate(draws):
+        win_axs = 0
         masks[np.where(boards == draw)] = True
         if np.all(masks, axis = 1).any() == True:
-            winning_board = np.where(np.all(masks, axis=1) ==True )[0][0]
+            win_axs = 1
+        elif np.all(masks, axis = 2).any() == True:
+            win_axs = 2
+        if win_axs !=0:
+            winning_board = np.where(np.all(masks, axis=win_axs) ==True )[0][0]
             winning_mask = np.squeeze(np.invert(masks[winning_board]))
             winning_sum = boards[winning_board,winning_mask].sum(dtype = np.int)
             winning_draw = draw
-            print(f'winner after {i} draws with number {draw} on board {winning_board}')
-            boards = np.delete(boards, winning_board, axis = 0)
-            masks = np.delete(masks, winning_board, axis = 0)
-            return(winning_sum * winning_draw, boards, masks, draws[i:])
-        if np.all(masks, axis = 2).any() == True:
-            winning_board = np.where(np.all(masks, axis=2) ==True )[0][0]
-            winning_mask = np.squeeze(np.invert(masks[winning_board]))
-            winning_sum = boards[winning_board,winning_mask].sum(dtype = np.int)
-            winning_draw = draw
-            print(f'winner after {i} draws with number {draw} on board {winning_board}')
+            # print(f'winner with draw {draw} and sum {winning_sum*winning_draw}')
             boards = np.delete(boards, winning_board, axis = 0)
             masks = np.delete(masks, winning_board, axis = 0)
             return(winning_sum * winning_draw, boards, masks, draws[i:])
@@ -41,9 +37,8 @@ boards = boards.reshape(100,5,5)
 masks = np.zeros((100,5,5), dtype = np.bool)
 
 sum, boards, masks, draws = play_game(boards, masks, draws)
-print(sum)
+print(f'first winning sum {sum}')
 while len(boards) != 0:
     sum, boards, masks, draws= play_game(boards, masks, draws)
-
-print(sum)
+print(f'last winning sum: {sum}')
 
