629d290765be3cf4a95b8567b8371f5312bc538e
Lina Kuhn <lina.kuhn@ymail.com>
2021-12-07 13:00:57 +0100

    faster solution

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -1,23 +1,24 @@
+import enum
 import numpy as np 
 
 def fuel(init_state):
-    fuel_cost = np.zeros_like(init_state, dtype = np.int)
-    for i in range(len(init_state)):
-        fuel_cost[i] = np.sum(np.abs(init_state-i))
-    # return [fuel_cost == min(fuel_cost)]
-    return min(fuel_cost)
+    cost = [np.sum(np.abs(init_state-i)) for i in range(len(init_state))]
+    return min(cost)
 
+### brute force it just because I can!
 def more_fuel(init_state):
     fuel_cost = np.zeros_like(init_state, dtype = np.int)
     for i in range(len(init_state)):
-        for k, state in enumerate(init_state):
-            fuel_cost[i] += np.sum(np.arange(1, np.abs(state-i)+1))
-    # return(np.where(fuel_cost == min(fuel_cost)))
+        fuel_cost[i] += np.sum([np.sum(np.arange(1, np.abs(init_state[k]-i)+1)) for k in range(len(init_state))])
     return min(fuel_cost)
+
+def calc_fuel(init_state):
+    mean = int(np.mean(init_state))
+    cost = [(np.sum(np.arange(1,np.abs(init_state[i]-mean)+1))) for i in range(len(init_state))]
+    return np.sum(cost)
 
 with open('input.txt', 'r') as file:
     init_state = np.loadtxt(file, delimiter = ',', dtype = np.int)
 
-test_state = np.array([16,1,2,0,4,2,7,1,2,14], dtype = np.int)
-
-print(more_fuel(test_state))
+print(fuel(init_state))
+print(calc_fuel(init_state))
